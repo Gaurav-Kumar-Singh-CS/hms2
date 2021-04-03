6480873221e0228592f6f93dbbc19de4b934d7b7
Gaurav Kumar Singh <474gaurav@gmail.com>
2021-04-03 11:10:16 +0530

    guest management

---
+++ guestmanagement/src/main/java/com/gaurav/guestmanagement/GuestManagementApplication.java
@@ -0,0 +1,13 @@
+package com.gaurav.guestmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuestManagementApplication.class, args);
+	}
+
+}
