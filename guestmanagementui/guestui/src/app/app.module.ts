422063b5c27a4325480be05844ef3fa30483bf93
Gaurav Kumar Singh <474gaurav@gmail.com>
2021-04-04 12:13:25 +0530

    added more components

--- guestmanagementui/guestui/src/app/app.module.ts
+++ guestmanagementui/guestui/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { RegisterComponent } from './register/register.component';
+import { SearchComponent } from './search/search.component';
+import { AllUsersComponent } from './all-users/all-users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    RegisterComponent,
+    SearchComponent,
+    AllUsersComponent
   ],
   imports: [
     BrowserModule,
