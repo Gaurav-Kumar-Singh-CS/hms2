96929a517c41c8e356bc5dc39450d0bce171d0e5
Gaurav Kumar Singh <474gaurav@gmail.com>
2021-04-07 00:26:03 +0530

    all microservice templates done

---
+++ room-management/src/test/java/com/gaurav/roommanagement/RoomManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaurav.roommanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
