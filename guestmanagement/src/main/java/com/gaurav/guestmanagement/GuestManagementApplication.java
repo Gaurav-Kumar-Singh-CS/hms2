9004e5d5eab7b02bd98d54c7cd0ac08c5554af77
Gaurav Kumar Singh <474gaurav@gmail.com>
2021-05-08 13:25:58 +0530

    swagger added

--- guestmanagement/src/main/java/com/gaurav/guestmanagement/GuestManagementApplication.java
+++ guestmanagement/src/main/java/com/gaurav/guestmanagement/GuestManagementApplication.java
@@ -3,8 +3,11 @@ package com.gaurav.guestmanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
+@EnableSwagger2
 public class GuestManagementApplication {
 
 	public static void main(String[] args) {
--- room-management/src/main/java/com/gaurav/roommanagement/RoomManagementApplication.java
+++ room-management/src/main/java/com/gaurav/roommanagement/RoomManagementApplication.java
@@ -3,7 +3,10 @@ package com.gaurav.roommanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class RoomManagementApplication {
 
 	public static void main(String[] args) {
